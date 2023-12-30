3df5a73f1cdc234e5ea1913a28557a2ba12ac7b9
Almond <42428221+almondnguyen@users.noreply.github.com>
2023-12-30 10:03:28 +0700

    build_kernel: add BSP_BUILD_ANDROID_OS=y
    because it is needed for building dtbs
    
    3 checks, really?
    
    Signed-off-by: Almond <42428221+almondnguyen@users.noreply.github.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,7 +9,7 @@ export PATH=${CLANG_TOOL_PATH}:${PATH//"${CLANG_TOOL_PATH}:"}
 export BSP_BUILD_FAMILY=sharkl3
 export DTC_OVERLAY_TEST_EXT=$(pwd)/tools/mkdtimg/ufdt_apply_overlay
 export DTC_OVERLAY_VTS_EXT=$(pwd)/tools/mkdtimg/ufdt_verify_overlay_host
-# export BSP_BUILD_ANDROID_OS=y
+export BSP_BUILD_ANDROID_OS=y
 
 make -C $(pwd) O=$(pwd)/out BSP_BUILD_DT_OVERLAY=y CC=clang LD=ld.lld ARCH=arm64 CLANG_TRIPLE=aarch64-linux-gnu- a3core_eur_open_defconfig -j4
 make -C $(pwd) O=$(pwd)/out BSP_BUILD_DT_OVERLAY=y CC=clang LD=ld.lld ARCH=arm64 CLANG_TRIPLE=aarch64-linux-gnu- -j4
